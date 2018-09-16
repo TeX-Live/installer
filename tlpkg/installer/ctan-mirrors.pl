dc96a46b03c4bcd93f31dfb85d4fd4373440e408
Norbert Preining <norbert@preining.info>
2018-09-15 20:37:02 -0400

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -27,7 +27,7 @@ $mirrors = {
     },
     'Iran' => {
       'http://ctan.asis.io/' => 1,
-      'http://ctan.yazd.ac.ir/' => 1,
+      'http://ctan.um.ac.ir/' => 1,
       'http://repo.iut.ac.ir/tex-archive/' => 1
     },
     'Japan' => {
@@ -112,8 +112,7 @@ $mirrors = {
       'http://piotrkosoft.net/pub/mirrors/CTAN/' => 1
     },
     'Portugal' => {
-      'ftp://ftp.di.uminho.pt/pub/ctan/' => 1,
-      'http://mirrors.up.pt/pub/CTAN/' => 1
+      'ftp://ftp.di.uminho.pt/pub/ctan/' => 1
     },
     'Romania' => {
       'http://mirrors.nxthost.com/ctan/' => 1
