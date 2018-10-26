f7af973ddcace38713f799cda6d0cda1d76ef63b
Norbert Preining <norbert@preining.info>
2018-10-25 20:37:02 -0400

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -22,8 +22,7 @@ $mirrors = {
       'http://mirrors.ustc.edu.cn/CTAN/' => 1
     },
     'India' => {
-      'http://ctan.imsc.res.in/' => 1,
-      'http://mirror.iopb.res.in/tex-archive/' => 1
+      'http://ctan.imsc.res.in/' => 1
     },
     'Iran' => {
       'http://ctan.asis.io/' => 1,
