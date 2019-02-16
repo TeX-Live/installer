127489599d14a3d748ff7791f28532e0448b09b3
Norbert Preining <norbert@preining.info>
2019-02-15 20:37:01 -0500

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -39,6 +39,7 @@ $mirrors = {
     },
     'Korea' => {
       'http://ftp.harukasan.org/CTAN/' => 1,
+      'http://ftp.kaist.ac.kr/pub/tex-archive/' => 1,
       'http://ftp.ktug.org/tex-archive/' => 1,
       'http://ftp.neowiz.com/CTAN/' => 1,
       'http://mirror.navercorp.com/CTAN/' => 1
