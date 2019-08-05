30f7bc5bc97d6cfd8f9f9f34396ae70c6864d122
Norbert Preining <norbert@preining.info>
2019-08-05 00:07:02 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -163,10 +163,5 @@ $mirrors = {
     'New Zealand' => {
       'http://mirror.aut.ac.nz/CTAN/' => 1
     }
-  },
-  'South America' => {
-    'Brazil' => {
-      'http://linorg.usp.br/CTAN/' => 1
-    }
   }
 };
