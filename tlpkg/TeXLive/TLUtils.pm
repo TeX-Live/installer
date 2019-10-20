410d048689c7092bed76ed0add1053404084750e
Norbert Preining <norbert@preining.info>
2019-10-20 22:07:01 +0000

    updating installer data

--- tlpkg/TeXLive/TLUtils.pm
+++ tlpkg/TeXLive/TLUtils.pm
@@ -1143,10 +1143,8 @@ sub copy {
   }
 
   if (-l "$infile") {
-    # the link target might be relative, so we need to start from
-    # the directory of $infile for the link
-    symlink (Cwd::abs_path(dirname($infile)) . "/" . readlink($infile), "$destdir/$filename")
-    || die "symlink(dirname($infile) . \"/\" . readlink($infile), $destdir/$filename) failed: $!";
+    symlink (readlink $infile, "$destdir/$filename")
+    || die "symlink(readlink $infile, $destdir/$filename) failed: $!";
   } else {
     if (! open (IN, $infile)) {
       warn "open($infile) failed, not copying: $!";
