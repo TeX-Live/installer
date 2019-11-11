92003fc2dc1c29351806f5f43d7c9b227cf01c6e
Norbert Preining <norbert@preining.info>
2019-11-11 01:07:01 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -55,7 +55,6 @@ $mirrors = {
       'https://download.nus.edu.sg/mirror/ctan/' => 1
     },
     'Taiwan' => {
-      'ftp://ftp.ccu.edu.tw/pub/tex/' => 1,
       'http://ftp.yzu.edu.tw/CTAN/' => 1
     }
   },
