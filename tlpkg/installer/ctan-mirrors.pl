c3806ed7d78ec47d9e217ebf3855b9741062db4b
Norbert Preining <norbert@preining.info>
2022-02-20 00:52:02 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -60,9 +60,6 @@ $mirrors = {
     'Philippines' => {
       'http://mirror.rise.ph/ctan/' => 1,
     },
-    'Singapore' => {
-      'https://download.nus.edu.sg/mirror/ctan/' => 1,
-    },
     'Taiwan' => {
       'https://ftp.ntou.edu.tw/ctan/' => 1,
     },
@@ -198,6 +195,7 @@ $mirrors = {
       'https://ctan.mirrors.hoobly.com/' => 1,
       'https://mirror.las.iastate.edu/tex-archive/' => 1,
       'https://mirror.math.princeton.edu/pub/CTAN/' => 1,
+      'https://mirror.mwt.me/ctan/' => 1,
       'https://mirrors.concertpass.com/tex-archive/' => 1,
       'https://mirrors.mit.edu/CTAN/' => 1,
       'https://mirrors.rit.edu/CTAN/' => 1,
