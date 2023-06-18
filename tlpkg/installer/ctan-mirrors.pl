15320742544d648f7a261badaf48edb82b6fb4e3
Norbert Preining <norbert@preining.info>
2023-06-18 00:07:15 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -25,9 +25,6 @@ $mirrors = {
       'https://mirrors.ustc.edu.cn/CTAN/' => 1,
       'https://mirrors.zju.edu.cn/CTAN/' => 1,
     },
-    'Hong Kong' => {
-      'https://mirror-hk.koddos.net/CTAN/' => 1,
-    },
     'India' => {
       'https://mirror.niser.ac.in/ctan/' => 1,
     },
