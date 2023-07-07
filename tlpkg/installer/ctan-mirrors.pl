3779a0019b0699cd24a9182bd673231784c8da99
Norbert Preining <norbert@preining.info>
2023-07-07 23:52:12 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -68,6 +68,9 @@ $mirrors = {
     'Czech Republic' => {
       'https://mirrors.nic.cz/tex-archive/' => 1,
     },
+    'Denmark' => {
+      'https://mirrors.dotsrc.org/ctan/' => 1,
+    },
     'Finland' => {
       'https://www.nic.funet.fi/pub/TeX/CTAN/' => 1,
       'https://www.texlive.info/CTAN/' => 1,
@@ -124,7 +127,6 @@ $mirrors = {
       'https://ctan.gust.org.pl/tex-archive/' => 1,
       'https://polish-mirror.evolution-host.com/ctan/' => 1,
       'https://repo.skni.umcs.pl/ctan/' => 1,
-      'https://sunsite.icm.edu.pl/pub/CTAN/' => 1,
     },
     'Portugal' => {
       'https://ftp.eq.uc.pt/software/TeX/' => 1,
