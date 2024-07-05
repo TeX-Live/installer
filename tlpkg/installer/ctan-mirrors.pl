eeb9dae4a5c86cafe4779d2abdd1400f6259be67
Norbert Preining <norbert@preining.info>
2024-07-05 23:52:13 +0000

    updating installer data

--- tlpkg/installer/ctan-mirrors.pl
+++ tlpkg/installer/ctan-mirrors.pl
@@ -37,7 +37,6 @@ $mirrors = {
       'http://ctan.asis.ai/' => 1,
     },
     'Japan' => {
-      'http://ring.airnet.ne.jp/archives/text/CTAN/' => 1,
       'https://ftp.jaist.ac.jp/pub/CTAN/' => 1,
       'https://ftp.kddilabs.jp/CTAN/' => 1,
       'https://ftp.yz.yamagata-u.ac.jp/pub/CTAN/' => 1,
